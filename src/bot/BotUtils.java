21f1cf3f4c507fe6075f15b7d79522d7d9b3a9d8
Anderson Rodrigues <anderson@idealogic.com.br>
2019-11-29 13:34:12 -0300

    sd

---
+++ src/bot/BotUtils.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bot;
+
+/**
+ *
+ * @author anderson
+ */
+public class BotUtils {
+
+    private static String token = "1029226417:AAGGRxjQUin91S4yJ13HJ7EByrXajwQ4O0U";
+    private static String link = "t.me/GeraldoPoo2Bot";
+    private static final String endpoint = "https://api.telegram.org/";
+    
+    
+}
